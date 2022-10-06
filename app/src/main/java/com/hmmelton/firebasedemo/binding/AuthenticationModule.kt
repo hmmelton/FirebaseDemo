390d8046c0b4f7d51cba55fbf16b6b08eedc2f96
Harrison Melton <hmmelton95@gmail.com>
2022-10-05 18:32:10 -0700

    setup Firebase to use Firebase Emulator on debug builds

--- app/src/main/java/com/hmmelton/firebasedemo/binding/AuthenticationModule.kt
+++ app/src/main/java/com/hmmelton/firebasedemo/binding/AuthenticationModule.kt
@@ -1,6 +1,7 @@
 package com.hmmelton.firebasedemo.binding
 
 import com.google.firebase.auth.FirebaseAuth
+import com.hmmelton.firebasedemo.BuildConfig
 import com.hmmelton.firebasedemo.utils.AuthManager
 import com.hmmelton.firebasedemo.utils.FirebaseAuthManager
 import dagger.Binds
@@ -15,6 +16,12 @@ abstract class AuthenticationModule {
 
     companion object {
         @Provides
-        fun provideFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
+        fun provideFirebaseAuth(): FirebaseAuth {
+            val auth = FirebaseAuth.getInstance()
+            if (BuildConfig.DEBUG) {
+                auth.useEmulator("10.0.2.2", 9099)
+            }
+            return auth
+        }
     }
 }
