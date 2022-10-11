4082b168ad08e5230aaae4d06d94fdd1fb0298df
Harrison Melton <hmmelton95@gmail.com>
2022-10-10 19:48:05 -0700

    changed parameter name

--- app/src/main/java/com/hmmelton/firebasedemo/data/model/Response.kt
+++ app/src/main/java/com/hmmelton/firebasedemo/data/model/Response.kt
@@ -3,4 +3,4 @@ package com.hmmelton.firebasedemo.data.model
 sealed interface Response
 
 object Success : Response
-class Error(val description: String): Response
+class Error(val message: String): Response
