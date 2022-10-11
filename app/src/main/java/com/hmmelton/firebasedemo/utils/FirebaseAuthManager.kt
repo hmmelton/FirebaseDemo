70293406ae007136c29d1195cbf00e48b1205cec
Harrison Melton <hmmelton95@gmail.com>
2022-10-10 19:48:57 -0700

    changed auth error messages

--- app/src/main/java/com/hmmelton/firebasedemo/utils/FirebaseAuthManager.kt
+++ app/src/main/java/com/hmmelton/firebasedemo/utils/FirebaseAuthManager.kt
@@ -6,43 +6,46 @@ import com.google.firebase.auth.FirebaseAuth
 import com.hmmelton.firebasedemo.data.model.Error
 import com.hmmelton.firebasedemo.data.model.Response
 import com.hmmelton.firebasedemo.data.model.Success
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-private const val ERROR_MESSAGE = "Error authenticating"
+private const val SIGN_IN_ERROR = "Failed to sign in"
+private const val REGISTRATION_ERROR = "Failed to register"
 private const val TAG = "FirebaseAuthManager"
 
 /**
  * [ViewModel] used to track/manage user authentication state.
  */
 class FirebaseAuthManager @Inject constructor(
-    private val auth: FirebaseAuth
+    private val auth: FirebaseAuth,
+    private val dispatcher: CoroutineDispatcher
 ) : AuthManager {
     override val isAuthenticated: Boolean
         get() = auth.currentUser != null
 
     override suspend fun signInWithEmail(email: String, password: String): Response {
-        return withContext(Dispatchers.IO) {
+        return withContext(dispatcher) {
             try {
                 auth.signInWithEmailAndPassword(email, password).await()
                 Success
             } catch (e: Exception) {
                 Log.e(TAG, "error signing in", e)
-                Error(e.message ?: ERROR_MESSAGE)
+                Error(SIGN_IN_ERROR)
             }
         }
     }
 
     override suspend fun registerWithEmail(email: String, password: String): Response {
-        return withContext(Dispatchers.IO) {
+        return withContext(dispatcher) {
             try {
                 auth.createUserWithEmailAndPassword(email, password).await()
                 Success
             } catch (e: Exception) {
                 Log.e(TAG, "error registering", e)
-                Error(e.message ?: ERROR_MESSAGE)
+                Error(REGISTRATION_ERROR)
             }
         }
     }
