0a37f8ed8df467b2c5cef960d950d9faa08e6da5
Harrison Melton <hmmelton95@gmail.com>
2022-10-13 08:13:20 -0700

    updated gitignore

--- app/src/main/java/com/hmmelton/firebasedemo/binding/AppComponent.kt
+++
@@ -1,9 +0,0 @@
-package com.hmmelton.firebasedemo.binding
-
-import com.hmmelton.firebasedemo.MainActivity
-import dagger.Component
-
-@Component(modules = [AuthenticationModule::class])
-interface AppComponent {
-    fun inject(activity: MainActivity)
-}
