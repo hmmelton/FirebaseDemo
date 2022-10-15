1fb6432176cba35586d9d03e1b73eb90faaf19c0
Harrison Melton <hmmelton95@gmail.com>
2022-10-15 08:51:28 -0700

    added FirebaseAnalytics injection

---
+++ app/src/main/java/com/hmmelton/firebasedemo/binding/MainModule.kt
@@ -0,0 +1,18 @@
+package com.hmmelton.firebasedemo.binding
+
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object MainModule {
+    @Singleton
+    @Provides
+    fun provideFirebaseAnalytics(): FirebaseAnalytics = Firebase.analytics
+}
