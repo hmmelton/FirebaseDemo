3d2ad0bf1e57337ee9c9c8c25a0ff78c000b1b7e
Harrison Melton <hmmelton95@gmail.com>
2022-10-27 08:54:41 -0700

    added ViewModel extension functions

---
+++ app/src/main/java/com/hmmelton/firebasedemo/ui/viewmodels/ViewModelExtensions.kt
@@ -0,0 +1,31 @@
+package com.hmmelton.firebasedemo.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+private val ui: CoroutineDispatcher = Dispatchers.Main
+private val io: CoroutineDispatcher =  Dispatchers.IO
+private val background: CoroutineDispatcher = Dispatchers.Default
+
+fun ViewModel.uiJob(block: suspend CoroutineScope.() -> Unit): Job {
+    return viewModelScope.launch(ui) {
+        block()
+    }
+}
+
+fun ViewModel.ioJob(block: suspend CoroutineScope.() -> Unit): Job {
+    return viewModelScope.launch(io) {
+        block()
+    }
+}
+
+fun ViewModel.backgroundJob(block: suspend CoroutineScope.() -> Unit): Job {
+    return viewModelScope.launch(background) {
+        block()
+    }
+}
