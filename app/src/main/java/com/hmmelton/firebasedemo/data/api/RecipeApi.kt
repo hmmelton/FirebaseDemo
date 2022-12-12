158c31ccf2c13b091e77a362c876ab9f365dfabe
Harrison Melton <hmmelton95@gmail.com>
2022-12-11 22:55:35 -0800

    added Retrofit interface and data class for recipes

---
+++ app/src/main/java/com/hmmelton/firebasedemo/data/api/RecipeApi.kt
@@ -0,0 +1,16 @@
+package com.hmmelton.firebasedemo.data.api
+
+import com.hmmelton.firebasedemo.data.model.Recipe
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * This API is used to fetch articles from the NYT API.
+ * @see [NYT API](https://developer.nytimes.com/apis)
+ */
+interface RecipeApi {
+    @GET("?orderBy=\"category\"")
+    suspend fun getRecipesForSection(
+        @Query("equalTo") category: String
+    ): List<Recipe>
+}
---
+++ app/src/main/java/com/hmmelton/firebasedemo/data/model/Recipe.kt
@@ -0,0 +1,43 @@
+package com.hmmelton.firebasedemo.data.model
+
+import java.util.Date
+
+/**
+ * Data class for a single recipe. The functions [equals] and [hashCode] are overridden as
+ * recommended by Android Studio, due to the presence of [Array] constructor parameters.
+ */
+data class Recipe(
+    val title: String,
+    val author: String,
+    val category: String,
+    val ingredients: Array<String>,
+    val instructions: Array<String>,
+    val createdAt: Date,
+    val updatedAt: Date
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Recipe
+
+        if (title != other.title) return false
+        if (author != other.author) return false
+        if (category != other.category) return false
+        if (!ingredients.contentEquals(other.ingredients)) return false
+        if (!instructions.contentEquals(other.instructions)) return false
+        if (createdAt != other.createdAt) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = title.hashCode()
+        result = 31 * result + author.hashCode()
+        result = 31 * result + category.hashCode()
+        result = 31 * result + ingredients.contentHashCode()
+        result = 31 * result + instructions.contentHashCode()
+        result = 31 * result + createdAt.hashCode()
+        return result
+    }
+}
