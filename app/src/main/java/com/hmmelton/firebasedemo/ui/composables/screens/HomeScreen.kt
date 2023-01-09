42fb280d45e89d8362c8bb033f3735bfe062f6d8
Harrison Melton <hmmelton95@gmail.com>
2023-01-09 13:28:34 -0800

    added header card to main screen

--- app/src/main/java/com/hmmelton/firebasedemo/ui/composables/screens/HomeScreen.kt
+++ app/src/main/java/com/hmmelton/firebasedemo/ui/composables/screens/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.hmmelton.firebasedemo.ui.composables.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,8 +10,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Card
 import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -21,6 +25,7 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -28,9 +33,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -77,6 +84,38 @@ fun HomeScreen(
                 verticalArrangement = Arrangement.spacedBy(8.dp),
                 contentPadding = PaddingValues(top = 16.dp, bottom = 16.dp)
             ) {
+                // Header
+                item {
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(200.dp)
+                            .padding(horizontal = 16.dp),
+                        elevation = 8.dp,
+                        backgroundColor = MaterialTheme.colors.secondaryVariant
+                    ) {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Icon(
+                                modifier = Modifier.requiredSize(50.dp),
+                                painter = painterResource(id = R.drawable.ic_menu_book),
+                                contentDescription = null,
+                                tint = Color.White
+                            )
+                            Text(
+                                modifier = Modifier
+                                    .padding(16.dp),
+                                text = stringResource(id = R.string.header_text),
+                                color = Color.White,
+                                style = MaterialTheme.typography.h6,
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                    }
+                }
+
                 // Categories label
                 item {
                     Text(
