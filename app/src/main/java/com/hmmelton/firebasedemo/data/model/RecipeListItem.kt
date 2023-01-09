3375bd4f42dfc23bdb11cf8b5049713b7dfb87d7
Harrison Melton <hmmelton95@gmail.com>
2023-01-09 12:06:57 -0800

    added Room, removed Storage

--- app/src/main/java/com/hmmelton/firebasedemo/data/model/RecipeListItem.kt
+++
@@ -1,9 +0,0 @@
-package com.hmmelton.firebasedemo.data.model
-
-/**
- * Data class used to display recipe information in a list
- */
-data class RecipeListItem(
-    val recipe: Recipe,
-    val thumbnailImage: String?
-)
